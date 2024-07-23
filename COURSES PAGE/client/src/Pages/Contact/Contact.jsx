660b645955e732959d5c6151d72b288a8632c3cd
alfredrubinstein <rubinsteinalfred@gmail.com>
2024-07-23 20:29:53 +0300

    cambios en login y register

--- COURSES PAGE/client/src/Pages/Contact/Contact.jsx
+++ COURSES PAGE/client/src/Pages/Contact/Contact.jsx
@@ -1,78 +1,30 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import styles from "./contact.module.css";
+import React from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
+import styles from './contact.module.css';
+import FormHeader from '../../Components/FormComponents/FormHeader/FormHeader';
+import FormInput from '../../Components/FormComponents/FormInput/FormInput';
+import FormSubmitButton from '../../Components/FormComponents/FormSubmitButton/FormSubmitButton';
 
-const Contact = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+export default function Contact() {
+  const methods = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
-    alert("Form submitted successfully");
+    console.log('Form data:', data);
+    // agregar aquí para manejar el envío del formulario, como enviar los datos a un servidor.
   };
 
   return (
-    <>
-      {/* Contact Form */}
-      <div className={styles.Contact_form}>
-        <div className={styles.row}>
-          <div className={styles.divIntermedio}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <h6>צור איתנו קשר עכשיו</h6>
-              <div className={styles.formControl}>
-                <label htmlFor="name">שם</label>
-                <input
-                  type="text"
-                  id="name"
-                  className={styles.formControl}
-                  placeholder="השם שלך"
-                  {...register("name", { required: "שם חובה" })}
-                />
-                {errors.name && <span>{errors.name.message}</span>}
-              </div>
-
-              <div className={styles.formControl}>
-                <label htmlFor="email">אמייל</label>
-                <input
-                  type="email"
-                  id="email"
-                  className={styles.formControl}
-                  placeholder="האמייל שלך"
-                  {...register("email", { required: "אמייל חובה" })}
-                />
-                {errors.email && <span>{errors.email.message}</span>}
-              </div>
-
-              <div className={styles.formControl}>
-                <label htmlFor="phone">מס' טלפון</label>
-                <input
-                  type="number"
-                  id="phone"
-                  className={styles.formControl}
-                  placeholder="הכנס מספר טלפון"
-                  {...register("phone", { required: "מספר טלפון חובה" })}
-                />
-                {errors.phone && <span>{errors.phone.message}</span>}
-              </div>
-
-              <div className={styles.formControl}>
-                <label htmlFor="message">איך נוכל לעזור</label>
-                <textarea
-                  rows="6"
-                  id="message"
-                  className={styles.formControl}
-                  placeholder="איך נוכל לעזור"
-                  {...register("message", { required: "הודעה חובה" })}
-                ></textarea>
-                {errors.message && <span>{errors.message.message}</span>}
-              </div>
-
-              <button type="submit" className={styles.btn}>שלח עכשיו</button>
-            </form>
-          </div>
-        </div>
+    <FormProvider {...methods}>
+      <div className={styles.container}>
+        <FormHeader type="contact" />
+        <form className={styles.form} onSubmit={methods.handleSubmit(onSubmit)}>
+          <FormInput type="text" title="Name" placeholder="Enter your name" name="name" />
+          <FormInput type="email" title="Email" placeholder="Enter your email" name="email" />
+          <FormInput type="text" title="Subject" placeholder="Enter the subject" name="subject" />
+          <FormInput type="textarea" title="Message" placeholder="Enter your message" name="message" />
+          <FormSubmitButton />
+        </form>
       </div>
-    </>
+    </FormProvider>
   );
-};
-
-export default Contact;
+}
