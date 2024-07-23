397192ef1e28409af60943c17d78734120481f24
alfredrubinstein <rubinsteinalfred@gmail.com>
2024-07-23 20:25:16 +0300

    cambios en login y register

--- COURSES PAGE/client/src/Pages/Login/Login.jsx
+++ COURSES PAGE/client/src/Pages/Login/Login.jsx
@@ -1,104 +1,24 @@
-import React, { useContext, useState } from 'react';
-import axios from 'axios';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
 import styles from './login.module.css';
-import Logo from '../../Components/Logo/Logo';
-import { AuthContext } from '../../contexts/AuthContext';
-import Modal from '../../Components/Modal/Modal';
+import FormButtons from '../../Components/FormComponents/FormButtons/FormButtons';
+import FormHeader from '../../Components/FormComponents/FormHeader/FormHeader';
+import FormInput from '../../Components/FormComponents/FormInput/FormInput';
+import FormSubmitButton from '../../Components/FormComponents/FormSubmitButton/FormSubmitButton';
 
-const Login = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const navigate = useNavigate();
-  const [bypassValidation, setBypassValidation] = useState(false);
-  const { login } = useContext(AuthContext);
-
-  const onSubmit = async (data) => {
-    if (bypassValidation) {
-      alert("כניסה ללא אימות");
-      navigate("/about");
-      return;
-    }
-
-    try {
-      const response = await axios.post("URL_TO_YOUR_API/login", data);
-      login(response.data);
-      alert("נכנסת בהצלחה");
-      navigate("/about");
-    } catch (error) {
-      console.error("Error logging in:", error);
-      alert("שגיאה בכניסה");
-    }
-  };
+export default function Login() {
+  const methods = useForm();
 
   return (
-    <>
-    {/* <Modal text="hola" type="onlyExit" active='isActive'/> */}
-    {/* <Modal text="hola como esta" type="onlyAccept" active='isActive'/> */}
-    <Modal text="hola estamos arreglando la pagina disculpe las molestias que esto pueda ocasionar" type="doubleButton" active='isActive'/>
-      {/* Login Form */}
-      <div className={styles.loginForm}>
-        <div className={styles.divIntermedio}>
-          <div className={styles.divInterno}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <h6>כניסה למשתמשים רשומים</h6>
-              <div className={styles.logoContainer}>
-                <Logo />
-              </div>
-              <div className={styles.formGroup}>
-                <label htmlFor="name">שם</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="name"
-                  placeholder="השם שלך"
-                  {...register("name", { required: "שם חובה" })}
-                />
-                {errors.name && <span>{errors.name.message}</span>}
-              </div>
-
-              <div className={styles.formGroup}>
-                <label htmlFor="email">אמייל</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  placeholder="האמייל שלך"
-                  {...register("email", { required: "אמייל חובה" })}
-                />
-                {errors.email && <span>{errors.email.message}</span>}
-              </div>
-
-              <div className={styles.formGroup}>
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="bypassValidation"
-                  checked={bypassValidation}
-                  onChange={() => setBypassValidation(!bypassValidation)}
-                />
-                <label className={styles.formLabelCheckbox} htmlFor="bypassValidation">
-                  עקוף אימות
-                </label>
-              </div>
-
-              <button type="submit" className={styles.btn}>
-                הכנס
-              </button>
-
-              <button
-                type="button"
-                className={styles.btnSecondary}
-                onClick={() => navigate("/register")}
-              >
-                משתמש חדש
-              </button>
-            </form>
-          </div>
-        </div>
+    <FormProvider {...methods}>
+      <div className={styles.container}>
+        <FormHeader type="login" />
+        <form className={styles.form}>
+          <FormInput type="text" title="Username" placeholder="Enter your username" name="username" />
+          <FormInput type="password" title="Password" placeholder="Enter your password" name="password" />
+          <FormSubmitButton />
+        </form>
       </div>
-    </>
+    </FormProvider>
   );
-};
-
-export default Login;
+}
--- COURSES PAGE/client/src/Pages/Register/Register.jsx
+++ COURSES PAGE/client/src/Pages/Register/Register.jsx
@@ -1,116 +1,24 @@
-import React, { useContext, useState } from 'react';
-import axios from 'axios';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
 import styles from './register.module.css';
-import Logo from '../../Components/Logo/Logo';
-import { AuthContext } from '../../contexts/AuthContext'; // Importa el contexto
+import FormHeader from '../../Components/FormComponents/FormHeader/FormHeader';
+import FormInput from '../../Components/FormComponents/FormInput/FormInput';
+import FormSubmitButton from '../../Components/FormComponents/FormSubmitButton/FormSubmitButton';
 
-const Register = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const navigate = useNavigate();
-  const [bypassValidation, setBypassValidation] = useState(false);
-  const { login } = useContext(AuthContext); // Usa el contexto
-
-  const onSubmit = async (data) => {
-    if (bypassValidation) {
-      alert("כניסה ללא אימות");
-      navigate("/about");
-      return;
-    }
-
-    try {
-      const response = await axios.post("URL_TO_YOUR_API/register", data);
-      login(response.data); // Guarda los datos de autenticación en el contexto
-      alert("נרשמת בהצלחה");
-      navigate("/about");
-    } catch (error) {
-      console.error("Error registering:", error);
-      alert("שגיאה בהרשמה");
-    }
-  };
+export default function Register() {
+  const methods = useForm();
 
   return (
-    <>
-      {/* Register Form */}
-      <div className={styles.containerForm}>
-      <div className={styles.register_form}>
-        <div className={styles.divIntermedio}>
-          <div className={styles.divInterno}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className={styles.logoContainer}>
-                <Logo />
-              </div>
-              <div className={styles.title}>הרשמה למשתמשים חדשים</div>
-              <div className={styles.formGroup}>
-                <label htmlFor="name">שם</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="name"
-                  placeholder="השם שלך"
-                  {...register("name", { required: "שם חובה" })}
-                />
-                {errors.name && <span>{errors.name.message}</span>}
-              </div>
-
-              <div className={styles.formGroup}>
-                <label htmlFor="email">אמייל</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  placeholder="האמייל שלך"
-                  {...register("email", { required: "אמייל חובה" })}
-                />
-                {errors.email && <span>{errors.email.message}</span>}
-              </div>
-
-              <div className={styles.formGroup}>
-                <label htmlFor="password">סיסמה</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="password"
-                  placeholder="הסיסמה שלך"
-                  {...register("password", { required: "סיסמה חובה" })}
-                />
-                {errors.password && <span>{errors.password.message}</span>}
-              </div>
-
-              <div className={styles.divCheckbox}>
-                <input
-                  type="checkbox"
-                  className={styles.checkbox}
-                  id="bypassValidation"
-                  checked={bypassValidation}
-                  onChange={() => setBypassValidation(!bypassValidation)}
-                />
-                <label className="form-check-label" htmlFor="bypassValidation">
-                  עקוף אימות
-                </label>
-              </div>
-<div className={styles.buttonsContainer}>
-              <button type="submit" className={styles.btn}>
-                הירשם
-              </button>
-
-
-              <button
-                type="button"
-                className={styles.btn_secondary}
-                onClick={() => navigate("/login")}
-              >
-                יש לך כבר חשבון?
-              </button>
-              </div>
-            </form>
-          </div>
-        </div>
+    <FormProvider {...methods}>
+      <div className={styles.container}>
+        <FormHeader type="register" />
+        <form className={styles.form}>
+          <FormInput type="text" title="Username" placeholder="Enter your username" name="username" />
+          <FormInput type="email" title="Email" placeholder="Enter your email" name="email" />
+          <FormInput type="password" title="Password" placeholder="Enter your password" name="password" />
+          <FormSubmitButton />
+        </form>
       </div>
-      </div>
-    </>
+    </FormProvider>
   );
-};
-
-export default Register;
+}
