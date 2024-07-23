7a3c540106757e2c106887a293ee937c66753d0f
alfredrubinstein <rubinsteinalfred@gmail.com>
2024-07-23 11:45:26 +0300

    arreglos en el readme

--- COURSES PAGE/client/README.js
+++ COURSES PAGE/client/README.js
@@ -1,7 +1,6 @@
 #to run the page:
-#1- open the terminal in the "COURSES PAGE" folder
+#1- open the terminal in the "client" folder
 #2- type "npm run moshico"
-#3- then open the browser and type "http://localhost:3000/" in the search bar
 
 
 
